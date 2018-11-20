9bb7edd5bfec3cab72df5fc7b4d91368f7974ebd
chipomwitu <chipomwitu88@gmail.com>
2018-11-20 23:10:37 +0300

    Added MovieDetail to core.urls.py + added MovieDetail view

--- MyMDB/django/core/views.py
+++ MyMDB/django/core/views.py
@@ -1,8 +1,13 @@
 from core.models import Movie
 
-from django.views.generic import ListView
+from django.views.generic import (
+    ListView, DetailView,
+)
 
 # class-based views
+
+class MovieDetail(DetailView):
+    model=Movie
 
 class MovieList(ListView):
     model=Movie
