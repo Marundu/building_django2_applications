288a37dbfa8ee70ff6b26ee76ebded8c4cc000e8
chipomwitu <chipomwitu88@gmail.com>
2019-01-25 11:45:14 +0300

    Added person detail view and template

--- MyMDB/django/core/views.py
+++ MyMDB/django/core/views.py
@@ -1,4 +1,4 @@
-from core.models import Movie
+from core.models import Movie, Person
 
 from django.views.generic import (
     ListView, DetailView,
@@ -12,3 +12,6 @@ class MovieDetail(DetailView):
 class MovieList(ListView):
     model=Movie
     #paginate_by=10
+
+class PersonDetail(DetailView):
+    queryset=Person.objects.all_with_prefetch_movies()
